299558d57c5606dfcd653949f39a30e2b105bd6f
takaosgb3 <takaosgb3@gmail.com>
2025-04-20 11:05:29 +0900

    initial commit

---
+++ pkg/json/json.go
@@ -0,0 +1,225 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+Copyright (C) 2024 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// /////////////////////////////////////////////////////////////////////////////
+// This plugin is a general json parser. It can be used to extract arbitrary
+// fields from a buffer containing json data.
+// /////////////////////////////////////////////////////////////////////////////
+package json
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strings"
+
+	"github.com/alecthomas/jsonschema"
+	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk"
+	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins"
+	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/extract"
+	"github.com/valyala/fastjson"
+)
+
+const (
+	PluginName        = "json"
+	PluginDescription = "implements extracting arbitrary fields from inputs formatted as JSON"
+	PluginContact     = "github.com/falcosecurity/plugins/"
+	PluginVersion     = "0.7.3"
+)
+
+type Plugin struct {
+	plugins.BasePlugin
+	jparser     fastjson.Parser
+	jdata       *fastjson.Value
+	jdataEvtnum uint64 // The event number jdata refers to. Used to know when we can skip the unmarshaling.
+	Config      PluginConfig
+}
+
+func (m *Plugin) Info() *plugins.Info {
+	return &plugins.Info{
+		Name:        PluginName,
+		Description: PluginDescription,
+		Contact:     PluginContact,
+		Version:     PluginVersion,
+	}
+}
+
+func (p *Plugin) InitSchema() *sdk.SchemaInfo {
+	reflector := jsonschema.Reflector{
+		// all properties are optional by default
+		RequiredFromJSONSchemaTags: true,
+		// unrecognized properties don't cause a parsing failures
+		AllowAdditionalProperties: true,
+	}
+	if schema, err := reflector.Reflect(&PluginConfig{}).MarshalJSON(); err == nil {
+		return &sdk.SchemaInfo{
+			Schema: string(schema),
+		}
+	}
+	return nil
+}
+
+func (m *Plugin) Init(config string) error {
+	// read configuration
+	m.Config.Reset()
+	json.Unmarshal([]byte(config), &m.Config)
+
+	// setup optional async extraction optimization
+	extract.SetAsync(m.Config.UseAsync)
+	return nil
+}
+
+func (m *Plugin) Fields() []sdk.FieldEntry {
+	return []sdk.FieldEntry{
+		{
+			Type: "string",
+			Name: "json.value",
+			Arg:  sdk.FieldEntryArg{IsRequired: true, IsKey: true},
+			Desc: "Extracts a value from a JSON-encoded input. Syntax is json.value[<json pointer>], where <json pointer> is a json pointer (see https://datatracker.ietf.org/doc/html/rfc6901)",
+		},
+		{
+			Type: "string",
+			Name: "json.obj",
+			Desc: "The full json message as a text string.",
+		},
+		{
+			Type: "string",
+			Name: "json.rawtime",
+			Desc: "The time of the event, identical to evt.rawtime.",
+		},
+		{
+			Type: "string",
+			Name: "jevt.value",
+			Arg:  sdk.FieldEntryArg{IsRequired: true, IsKey: true},
+			Desc: "Alias for json.value, provided for backwards compatibility.",
+		},
+		{
+			Type: "string",
+			Name: "jevt.obj",
+			Desc: "Alias for json.obj, provided for backwards compatibility.",
+		},
+		{
+			Type: "string",
+			Name: "jevt.rawtime",
+			Desc: "Alias for json.rawtime, provided for backwards compatibility.",
+		},
+	}
+}
+
+func (m *Plugin) Extract(req sdk.ExtractRequest, evt sdk.EventReader) error {
+	reader := evt.Reader()
+
+	// As a very quick sanity check, only try to extract all if
+	// the first character is '{' or '['
+	data := []byte{0}
+	_, err := reader.Read(data)
+	if err != nil {
+		return err
+	}
+	if !(data[0] == '{' || data[0] == '[') {
+		return fmt.Errorf("invalid json format")
+	}
+
+	// Decode the json, but only if we haven't done it yet for this event
+	if evt.EventNum() != m.jdataEvtnum {
+		_, err := reader.Seek(0, io.SeekStart)
+		if err != nil {
+			return err
+		}
+
+		data, err = ioutil.ReadAll(reader)
+		if err != nil {
+			return err
+		}
+
+		// Try to parse the data as json
+		m.jdata, err = m.jparser.ParseBytes(data)
+		if err != nil {
+			return err
+		}
+		m.jdataEvtnum = evt.EventNum()
+	}
+
+	switch req.FieldID() {
+	case 3: // jevt.value
+		fallthrough
+	case 0: // json.value
+		val := m.jdata
+
+		arg := req.ArgKey()
+		if len(arg) == 0 {
+			req.SetValue(string(val.MarshalTo(nil)))
+			return nil
+		}
+		if arg[0] == '/' {
+			arg = arg[1:]
+		}
+
+		// walk the object using the json pointer syntax (RFC 6901)
+		pointer := strings.Split(arg, "/")
+		for _, key := range pointer {
+			key = strings.Replace(key, "~1", "/", -1)
+			key = strings.Replace(key, "~0", "~", -1)
+			val = val.Get(key)
+			if val == nil {
+				return nil
+			}
+		}
+
+		if val.Type() == fastjson.TypeString {
+			str, err := val.StringBytes()
+			if err != nil {
+				return err
+			}
+			req.SetValue(string(str))
+		} else {
+			req.SetValue(string(val.MarshalTo(nil)))
+		}
+	case 4: // jevt.obj
+		fallthrough
+	case 1: // json.obj
+		// If we skipped the deserialization, we have to read
+		// the event data.
+		if len(data) == 1 {
+			_, err := reader.Seek(0, io.SeekStart)
+			if err != nil {
+				return err
+			}
+
+			data, err = ioutil.ReadAll(reader)
+			if err != nil {
+				return err
+			}
+		}
+		var out bytes.Buffer
+		err = json.Indent(&out, data, "", "  ")
+		if err != nil {
+			return err
+		}
+		req.SetValue(out.String())
+	case 5: // jevt.rawtime
+		fallthrough
+	case 2: // json.rawtime
+		req.SetValue(fmt.Sprintf("%d", evt.Timestamp()))
+	default:
+		return fmt.Errorf("no known field: %s", req.Field())
+	}
+
+	return nil
+}
---
+++ pkg/json/json_test.go
@@ -0,0 +1,290 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+Copyright (C) 2023 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package json
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+	"time"
+	"unsafe"
+
+	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk"
+)
+
+type testEventReader struct {
+	num      uint64
+	jsonData string
+	time     time.Time
+}
+
+func (t *testEventReader) EventNum() uint64 {
+	return t.num
+}
+
+func (t *testEventReader) Timestamp() uint64 {
+	return uint64(t.time.UnixNano())
+}
+
+func (t *testEventReader) Reader() io.ReadSeeker {
+	return strings.NewReader(t.jsonData)
+}
+
+type testExtractRequest struct {
+	fieldID    uint64
+	fieldType  uint32
+	arg        string
+	argIndex   uint64
+	argPresent bool
+	field      string
+	isList     bool
+	value      interface{}
+}
+
+func (t *testExtractRequest) FieldID() uint64 {
+	return t.fieldID
+}
+
+func (t *testExtractRequest) FieldType() uint32 {
+	return t.fieldType
+}
+
+func (t *testExtractRequest) Field() string {
+	return t.field
+}
+
+func (t *testExtractRequest) ArgKey() string {
+	return t.arg
+}
+
+func (t *testExtractRequest) ArgIndex() uint64 {
+	return t.argIndex
+}
+
+func (t *testExtractRequest) ArgPresent() bool {
+	return t.argPresent
+}
+
+func (t *testExtractRequest) IsList() bool {
+	return t.isList
+}
+
+func (t *testExtractRequest) SetValue(v interface{}) {
+	t.value = v
+}
+
+func (t *testExtractRequest) SetPtr(unsafe.Pointer) {
+	// do nothing
+}
+
+func TestExtractValue(t *testing.T) {
+	var s string
+	var ok bool
+	var err error
+	testEvent := &testEventReader{
+		num:  1,
+		time: time.Now(),
+		jsonData: `{
+			"list":[
+				{
+					"intvalue":1,
+					"floatvalue":2.5
+				}
+			],
+			"value":"hello",
+			"~/escaped":"hello\"2"
+		}`,
+	}
+	testRequest := &testExtractRequest{
+		fieldID:   0,
+		field:     "json.value",
+		arg:       "",
+		fieldType: sdk.FieldTypeCharBuf,
+		value:     nil,
+	}
+	e := &Plugin{}
+
+	// invalid json pointer
+	testRequest.arg = "invalid_pointer"
+	if err := e.Extract(testRequest, testEvent); err != nil {
+		t.Error(err)
+	}
+
+	// valid json pointer with string value
+	testRequest.arg = "/value"
+	err = e.Extract(testRequest, testEvent)
+	if err != nil {
+		t.Error(err)
+	}
+	if s, ok = testRequest.value.(string); !ok {
+		t.Errorf("expected string value")
+	}
+	if s != "hello" {
+		t.Errorf("expected value %s, but found %s", "hello", s)
+	}
+
+	// valid json pointer with string value, nesting, and int conversion
+	testRequest.arg = "/list/0/intvalue"
+	err = e.Extract(testRequest, testEvent)
+	if err != nil {
+		t.Error(err)
+	}
+	if s, ok = testRequest.value.(string); !ok {
+		t.Errorf("expected string value")
+	}
+	if s != "1" {
+		t.Errorf("expected value %s, but found %s", "1", s)
+	}
+
+	// valid json pointer with u64 value, nesting, and float conversion
+	testRequest.arg = "/list/0/floatvalue"
+	err = e.Extract(testRequest, testEvent)
+	if err != nil {
+		t.Error(err)
+	}
+	if s, ok = testRequest.value.(string); !ok {
+		t.Errorf("expected string value")
+	}
+	if s != "2.5" {
+		t.Errorf("expected value %s, but found %s", "2.5", s)
+	}
+
+	// test jevt.value alias too
+	testRequest.fieldID = 3
+	testRequest.field = "jevt.value"
+
+	// json pointer with escaping
+	testRequest.arg = "/~0~1escaped"
+	err = e.Extract(testRequest, testEvent)
+	if err != nil {
+		t.Error(err)
+	}
+	if s, ok = testRequest.value.(string); !ok {
+		t.Errorf("expected string value")
+	}
+	if s != "hello\"2" {
+		t.Errorf("expected value %s, but found %s", "hello\"2", s)
+	}
+
+	// empty json pointer
+	testRequest.arg = ""
+	err = e.Extract(testRequest, testEvent)
+	if err != nil {
+		t.Error(err)
+	}
+	if s, ok = testRequest.value.(string); !ok {
+		t.Errorf("expected string value")
+	}
+	minimized := "{\"list\":[{\"intvalue\":1,\"floatvalue\":2.5}],\"value\":\"hello\",\"~/escaped\":\"hello\\\"2\"}"
+	if s != minimized {
+		t.Errorf("expected value %s, but found %s", minimized, s)
+	}
+}
+
+func TestExtractObject(t *testing.T) {
+	var s string
+	var ok bool
+	var err error
+	testIndentedJSON := "{\n  \"value\": \"hello\"\n}"
+	testEvent := &testEventReader{
+		num:      1,
+		time:     time.Now(),
+		jsonData: `{"value":"hello"}`,
+	}
+	testRequest := &testExtractRequest{
+		fieldID:   1,
+		field:     "json.object",
+		arg:       "",
+		fieldType: sdk.FieldTypeCharBuf,
+		value:     nil,
+	}
+	e := &Plugin{}
+
+	// extract object with json.obj
+	err = e.Extract(testRequest, testEvent)
+	if err != nil {
+		t.Error(err)
+	}
+	if s, ok = testRequest.value.(string); !ok {
+		t.Errorf("expected string value")
+	}
+	if s != testIndentedJSON {
+		t.Errorf("expected value '%s', but found '%s'", testIndentedJSON, s)
+	}
+
+	// extract object with jevt.obj
+	testRequest.fieldID = 4
+	testRequest.field = "jevt.obj"
+	err = e.Extract(testRequest, testEvent)
+	if err != nil {
+		t.Error(err)
+	}
+	if s, ok = testRequest.value.(string); !ok {
+		t.Errorf("expected string value")
+	}
+	if s != testIndentedJSON {
+		t.Errorf("expected value '%s', but found '%s'", testIndentedJSON, s)
+	}
+}
+
+func TestExtractRawtime(t *testing.T) {
+	var ts string
+	var ok bool
+	var err error
+	testEvent := &testEventReader{
+		num:      1,
+		time:     time.Now(),
+		jsonData: "{}",
+	}
+	testTimeStr := fmt.Sprintf("%d", testEvent.time.UnixNano())
+	testRequest := &testExtractRequest{
+		fieldID:   2,
+		field:     "json.rawtime",
+		arg:       "",
+		fieldType: sdk.FieldTypeUint64,
+		value:     nil,
+	}
+	e := &Plugin{}
+
+	// extract object with json.rawtime
+	err = e.Extract(testRequest, testEvent)
+	if err != nil {
+		t.Error(err)
+	}
+	if ts, ok = testRequest.value.(string); !ok {
+		t.Errorf("expected uint64 value")
+	}
+	if ts != testTimeStr {
+		t.Errorf("expected value '%s', but found '%s'", testTimeStr, ts)
+	}
+
+	// extract object with jevt.rawtime
+	testRequest.fieldID = 5
+	testRequest.field = "jevt.rawtime"
+	err = e.Extract(testRequest, testEvent)
+	if err != nil {
+		t.Error(err)
+	}
+	if ts, ok = testRequest.value.(string); !ok {
+		t.Errorf("expected uint64 value")
+	}
+	if ts != testTimeStr {
+		t.Errorf("expected value '%s', but found '%s'", testTimeStr, ts)
+	}
+}
